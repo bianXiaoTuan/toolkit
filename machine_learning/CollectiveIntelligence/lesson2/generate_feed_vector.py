4ef0a4c4dfccb2a48401025dd3ce76a01ca641bb
陈欢 <chenhuan@chenhuandeMacPro.local>
2016-09-23 09:11:21 +0800

    重构generate_feeeed_vector脚本

--- machine_learning/CollectiveIntelligence/lesson2/generate_feed_vector.py
+++ machine_learning/CollectiveIntelligence/lesson2/generate_feed_vector.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #_*_ encoding=utf-8 _*_
 
-import feedparser
 import re
+import feedparser
 
 def get_words(sentence):
     '''分词
@@ -10,86 +10,120 @@ def get_words(sentence):
     words = re.compile(r'[^A-Z^a-z]').split(sentence)
     return [word.lower() for word in words if word != '']
 
-
 def get_wordcouts(url):
     '''返回RSS订阅源的标题和包含单词计数情况字典
     '''
-    word_counts = {}
-
     d = feedparser.parse(url)
     entries = d.entries
 
     if 'title' in d.feed :
-        blog_title = d.feed.title
+        title = d.feed.title
     else :
-        blog_title = url
+        title = url
 
+    wordcounts = {}
     for entry in entries:
-        words = [word.lower() for word in get_words(entry['title']) if word != '']
+        words = get_words(entry['title'])
 
         for word in words:
-            word_counts.setdefault(word, 0)
-            word_counts[word] += 1
+            wordcounts.setdefault(word, 0)
+            wordcounts[word] += 1
 
-    return blog_title,word_counts
+    return title,wordcounts
 
-def build_word_list(word_blog_counts, blog_count, bottom, top):
-    '''从word_counts中提取word_list
+def get_wordlist(word_blog_map, blog_num, lower_limit, upper_limit):
+    '''从word_counts中提取wordlist
+
+    :param word_blog_map: 出现word的blog数
+    :param blog_num: blog数
+    :param lower_limit: word出现频率下限
+    :param upper_limit: word出现频率上限
+    :return: [] 符合上下限单词表
     '''
-    word_list = []
-    length = len(word_blog_counts)
-    for word,count in word_blog_counts.items():
-        frac = float(count) / blog_count
+    wordlist = []
+    
+    length = len(word_blog_map)
+    for word,count in word_blog_map.items():
+        frac = float(count) / blog_num
 
-        if bottom <= frac and frac <= top:
-            word_list.append(word)
+        if lower_limit <= frac and frac <= upper_limit:
+            wordlist.append(word)
 
-    return word_list
+    return wordlist
 
-def build_feed_vector(file):
+def get_urls(feed_list_file, count=5):
     '''
     '''
-    blog_count = 0
-    total_word_blog_counts = {}
-    total_word_counts = {}
+    fd = open(feed_list_file)
+    return [url.strip() for url in fd.readlines() if url.strip() != ''][0:count]
 
-    # 读取RSS源, 生成各博客关键词的wordcounts
-    fd = open(file)
-    for url in fd.readlines():
-        blog_title,word_counts = get_wordcouts(url)
-        total_word_counts[blog_title] = word_counts
+def statics_rss_urls(urls):
+    '''统计rss_urls中关键词信息
+
+    :param urls: [] rss url列表
+    :return:
+     word_blog_map: {}   # 出现word的blog数
+     blog_word_map: {}   # 每篇blog的word数
+     blog_num: int       # 博客数
+    '''
+    word_blog_map = {}    # 出现word的blog数
+    blog_word_map = {}    # 每篇blog的word数
+    blog_num = 0          # 博客数
+
+    for url in urls:
+        print url + ' begin'
+
+        title,wordcounts = get_wordcouts(url)
+        blog_word_map[title] = wordcounts
 
         # 每个word有多少个blog
-        for word,count in word_counts.items():
-            total_word_blog_counts.setdefault(word, 0)
-            if count > 0:
-                total_word_blog_counts[word] += 1
+        for word,count in wordcounts.items():
+            word_blog_map.setdefault(word, 0)
+            word_blog_map[word] += 1 if count > 0 else 0
 
-        blog_count += 1
+        blog_num += 1
 
-    # 构建word_list
-    word_list = build_word_list(total_word_blog_counts, blog_count, 0.1, 0.5)
+    return word_blog_map,blog_word_map,blog_num
 
-    # 构建blog 和 word矩阵写入文件blog_data.txt
-    out = open('blog_data.txt', 'w')
+def write_statics_to_file(result_file, blog_word_map, wordlist):
+    '''将统计结果矩阵写入文件result_file中
+    '''
+    out = open(result_file, 'w')
+
+    # 第一行标识单词
     out.write('Blog')
-    for word in word_list:
+    for word in wordlist:
         out.write('\t%s' % word)
 
     out.write('\n')
 
-    for title,word_counts in total_word_counts.items():
+    for title,wordcounts in blog_word_map.items():
+        # 第一列blog名字
         out.write(title)
 
-        for word in word_list:
-            if word in word_counts:
-                out.write('\t%d' % word_counts[word])
+        for word in wordlist:
+            if word in wordcounts:
+                out.write('\t%d' % wordcounts[word])
             else:
                 out.write('\t%d' % 0)
 
         out.write('\n')
-
     out.close()
 
+def get_feed_vector(feed_list_file):
+    '''生成博客关键词统计
+    '''
+    # 从文件获取RSS URL
+    urls = get_urls(feed_list_file, 1)
+
+    # 统计blog_word_map, word_blog_map 和 blog_num
+    blog_word_map,word_blog_map,blog_num = statics_rss_urls(urls)
+
+    # 构建wordlist
+    wordlist = get_wordlist(word_blog_map, blog_num, 0.1, 0.5)
+
+    # 构建blog 和 word矩阵写入文件blog_data.txt
+    write_statics_to_file('blog_data.txt', blog_word_map, wordlist)
+
 if __name__ == '__main__':
-    build_feed_vector('feedlist')
+    get_feed_vector('feedlist')
